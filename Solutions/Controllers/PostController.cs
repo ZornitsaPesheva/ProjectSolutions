9d27da7636a7d32dfefc7592a9158cdb2acc1f7a
Pilgrimage <nanobot149@gmail.com>
2016-12-22 03:18:56 +0200

    Направени са Edit & Delete Posts. Edit - с ограничения.

--- Solutions/Controllers/PostController.cs
+++ Solutions/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Solutions.Enums;
@@ -71,20 +72,187 @@ namespace Solutions.Controllers
             return View(model);
         }
 
-        // GET: Post/Delete
-
-
 
 
         // GET: Post/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new ApplicationDbContext())
+            {
+                var model = new PostViewModel();
+
+                // Get post from the database
+                var result = database.Posts
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .Include(b => b.Chapter)
+                    .First();
+
+                // Check if post exists
+                if (result == null)
+                {
+                    return HttpNotFound();
+                }
+
+                model.Title = result.Title;
+                model.Link = result.Link;
+                //model.Language = result.LanguageId;
+                //model.Verify = result.Verified;
+
+                model.ChapterId = result.ChapterId;
+                model.AutorId = result.AuthorId;
+
+                model.Languages = Enum.GetNames(typeof(Languages)).ToList();
+                model.Verified = Enum.GetNames(typeof(Verify)).ToList();
+
+                return View(model);
+            }
+        }
 
 
 
+
+
+        //
         // POST: Post/Edit
+        [HttpPost]
+        [ActionName("Edit")]
+        //public ActionResult Edit(int? id, FormCollection editedPost)
+        public ActionResult Edit(PostViewModel editedPost, int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Check if model state is valid
+            if (ModelState.IsValid)
+            {
+                using (var database = new ApplicationDbContext())
+                {
+                    var model = new PostViewModel();
+
+                    // Get post from the database
+                    var solution = database.Posts
+                        .Where(a => a.Id == id)
+                        .Include(a => a.Author)
+                        .Include(b => b.Chapter)
+                        .First();
+
+                    // Set post title and content
+                    solution.Title = editedPost.Title;
+                    solution.Link = editedPost.Link;
+                    //solution.ChapterId = result.ChapterId;
+                    //solution.AuthorId = this.User.Identity.Name;
+                    //solution.LanguageId = Enum.GetNames(typeof(Languages)).ToString();
+                    //solution.Verified = Enum.GetNames(typeof(Verify)).ToString();
+
+                    // Save post state in database
+                    database.Entry(solution).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    // Redirect to the index page
+                    //return RedirectToAction("Index");
+                    return RedirectToAction("ListPosts", "Home", new { @chapterId = solution.ChapterId });
+                }
+            }
+
+            // If model state is invalid, return the same view
+            return View();
+        }
 
 
 
+        // GET: Post/Delete
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            var model = new PostViewModel();
 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new ApplicationDbContext())
+            {
+                // Get post from database
+                var post = database.Posts
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (!IsUserAuthorizedToEdit(post))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                // Check if post exists
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+                model.Title = post.Title;
+                model.Link = post.Link;
+                model.Language = post.LanguageId;
+                // Pass post to view
+                return View(model);
+            }
+        }
+
+
+
+        // POST: Post/Delete
+        [HttpPost, Authorize]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            var model = new PostViewModel();
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new ApplicationDbContext())
+            {
+                // Get post from database
+                var post = database.Posts
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+                var ChapId = post.ChapterId;
+
+                // Check if post exists
+                if (post == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Delete post from database
+                database.Posts.Remove(post);
+                database.SaveChanges();
+
+                // Redirect to index page
+                //return RedirectToAction("Index");
+                return RedirectToAction("ListPosts", "Home", new { @chapterId = ChapId });
+            }
+        }
+
+
+        
+        private bool IsUserAuthorizedToEdit(Post post)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = post.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
 
     }
 }
